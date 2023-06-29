b1fd5da9bcfedd9e89f42bd00684b41f10c1e0f3
leomujo <111306068+leomujo@users.noreply.github.com>
2023-06-29 18:32:18 -0500

    updating manifest

---
+++ Extension/CFextension/manifest.js
@@ -0,0 +1,16 @@
+{
+    "manifest_version": 3,
+    "name": "Usernames taker",
+    "Version":"1.0.1",
+    "description":"Allows users to read and store usernames from forem websites. It provides a convenient way to save and manage usernames for future reference.",
+    "default_locale": "en",
+    "autor":"David Mujica"
+    "icons": {
+        "16": "icon16.png",
+        "32": "icon32.png",
+        "48": "icon48.png",
+        "128": "icon128.png"
+      },
+      
+
+}
